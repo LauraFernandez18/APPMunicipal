f75f4d4d381aa940f8765aecb5af8251c6624665
JuanCarlosGundin <juancarlosgr9898@gmail.com>
2021-11-23 15:27:34 +0100

    Base de datos

---
+++ bd_app/bd_app_tbl_inscripciones.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bd_app
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbl_inscripciones`
+--
+
+DROP TABLE IF EXISTS `tbl_inscripciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_inscripciones` (
+  `id_inscripcion` int NOT NULL AUTO_INCREMENT,
+  `id_evento` int DEFAULT NULL,
+  `id_usuario` int DEFAULT NULL,
+  `fecha_inscripcion` date DEFAULT NULL,
+  `hora_inscripcion` time DEFAULT NULL,
+  PRIMARY KEY (`id_inscripcion`),
+  KEY `fk_inscripcion_usuarios_idx` (`id_usuario`),
+  KEY `fk_inscripcion_eventos_idx` (`id_evento`),
+  CONSTRAINT `fk_inscripcion_eventos` FOREIGN KEY (`id_evento`) REFERENCES `tbl_eventos` (`id_evento`),
+  CONSTRAINT `fk_inscripcion_usuarios` FOREIGN KEY (`id_usuario`) REFERENCES `tbl_usuarios` (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_inscripciones`
+--
+
+LOCK TABLES `tbl_inscripciones` WRITE;
+/*!40000 ALTER TABLE `tbl_inscripciones` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_inscripciones` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-11-23 15:23:47
