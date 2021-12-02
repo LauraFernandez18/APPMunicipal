1ff10a957315e24bc3359ebb16eb87d0bdc2b186
JuanCarlosGundin <juancarlosgr9898@gmail.com>
2021-12-02 18:35:00 +0100

    vista de registrados

--- view/registered.admin.php
+++ view/registered.admin.php
@@ -1,7 +1,12 @@
 <?php
 include '../services/config.php';
 include '../services/conexion.php';
-$sentencia=$pdo->prepare("SELECT * FROM tbl_eventos");
+$idEvent=$_POST['id'];
+$sentencia=$pdo->prepare("SELECT * FROM tbl_usuarios
+inner join tbl_inscripciones
+on tbl_usuarios.id_usuario=tbl_inscripciones.id_usuario
+where tbl_inscripciones.id_evento=?");
+$sentencia->bindParam(1, $idEvent);
 $sentencia->execute();
 $eventos=$sentencia->fetchAll(PDO::FETCH_ASSOC);?>
 <!DOCTYPE html>
@@ -22,9 +27,12 @@ $eventos=$sentencia->fetchAll(PDO::FETCH_ASSOC);?>
 <?php
     foreach ($eventos as $producto){
         ?>
-        <p><?php echo"{$producto['lugar_evento']}";?><p>
-        <p><?php echo"{$producto['fecha_evento']}";?><p>
-        <p><?php echo"{$producto['hora_evento']}";?><p>
+        <p><?php echo"{$producto['nom_usuario']}";?><p>
+        <p><?php echo"{$producto['apellido_usuario']}";?><p>
+        <p><?php echo"{$producto['telf_usuario']}";?><p>
+        <p><?php echo"{$producto['dni_usuario']}";?><p>
+         <!--Esto de aqui abajo haz un echo para pasarlo al modificar no lo muestres que es el id, te lo coloco para que lo veas solo -->
+        <p><?php echo"{$producto['id_usuario']}";?><p>
     <?php 
     }
      ?>
